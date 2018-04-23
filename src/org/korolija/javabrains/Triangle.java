ec2cc55a1cd33592c6aefa52829c94455ff9fce2
Koroman91 <korolija91@yahoo.com>
2018-04-23 18:16:07 +0200

    Forth spring commit

--- src/org/korolija/javabrains/Triangle.java
+++ src/org/korolija/javabrains/Triangle.java
@@ -2,50 +2,46 @@ package org.korolija.javabrains;
 
 public class Triangle {
 	
-	
-	private String type;
-	
-	private int height;
-	
-	public int getHeight() {
-		return height;
+	public Point getPointA() {
+		return pointA;
 	}
 
 
-	public void setHeight(int height) {
-		this.height = height;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
 
-	public Triangle(String type){
-		this.type = type;
-	}
-	
-	public Triangle(int height)
-	{
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height)
-	{
-		this.type = type;
-		this.height = height;
-	}
-	
-	
-	public String getType() {
-		return type;
+	public Point getPointB() {
+		return pointB;
 	}
 
 
-/*	public void setType(String type) {
-		this.type = type;
-	}*/
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
 
 	public void draw()
 	{
-		System.out.println(getType() + " Triangle drawn of height " + getHeight());
+		System.out.println("Point A = (" + getPointA().getX() + ", " +getPointA().getY() +")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " +getPointB().getY() +")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " +getPointC().getY() +")");
 	}
 
 }
