75d182005b15fd2a48a2b2d8950b84a70ea9317d
Koroman91 <korolija91@yahoo.com>
2018-04-24 15:52:51 +0200

    Lifecycle Callbacks commit

--- src/org/korolija/javabrains/Triangle.java
+++ src/org/korolija/javabrains/Triangle.java
@@ -2,21 +2,20 @@ package org.korolija.javabrains;
 
 import java.util.List;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class Triangle implements ApplicationContextAware, BeanNameAware {
+
+public class Triangle implements InitializingBean, DisposableBean {
 	
-/*	
+
     public Point getPointA() {
 		return pointA;
 	}
 
 	public void setPointA(Point pointA) {
 		this.pointA = pointA;
-	}*/
+	}
 
 	public Point getPointB() {
 		return pointB;
@@ -34,27 +33,51 @@ public class Triangle implements ApplicationContextAware, BeanNameAware {
 		this.pointC = pointC;
 	}
 
-	private List<Point> points;
+	private Point pointA;
     private Point pointB;
     private Point pointC;
-    private ApplicationContext context = null; 
+  
 
 	public void draw()
 	{
-		//System.out.println("Point A = (" + getPointA().getX() + ", " +getPointA().getY() +")");
+		System.out.println("Point A = (" + getPointA().getX() + ", " +getPointA().getY() +")");
 		System.out.println("Point B = (" + getPointB().getX() + ", " +getPointB().getY() +")");
 		System.out.println("Point C = (" + getPointC().getX() + ", " +getPointC().getY() +")");
 	}
 
-	public void setApplicationContext(ApplicationContext context) 
+/*	public void setApplicationContext(ApplicationContext context) 
 			throws BeansException {
 		// TODO Auto-generated method stub
 		this.context = context;
 	}
-
+*/
 	public void setBeanName(String beanName) {
 		// TODO Auto-generated method stub
 		System.out.println("Bean name is: " +beanName);
 	}
 
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("InitializingBean init method called for Triangle");
+	}
+
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("DisposableBean destroy method called for Triangle");
+
+	}
+
+	
+	public void myInit()
+	{
+		System.out.println("My init method called for Triangle");
+	}
+	
+	public void cleanUp()
+	{
+		System.out.println("My cleanup method called for Triangle");
+
+	}
+	
+	
 }
