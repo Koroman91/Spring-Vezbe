478f8ef0868de7e0c6c4791aca31f752ee34da1e
Koroman91 <korolija91@yahoo.com>
2018-04-25 15:29:54 +0200

    Event Handling commit

--- src/org/korolija/javabrains/Circle.java
+++ src/org/korolija/javabrains/Circle.java
@@ -6,16 +6,19 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.portlet.bind.annotation.ResourceMapping;
 
 @Component
-public class Circle implements Shape {
+public class Circle implements Shape, ApplicationEventPublisherAware {
 	
 	private Point center;
 	@Autowired
 	private MessageSource messageSource;
+	private ApplicationEventPublisher publisher;
 
 
 
@@ -46,7 +49,8 @@ public void draw() {
 	System.out.println(this.getMessageSource().getMessage("drawing.circle", null, "Default Drawing Message", null));
 	System.out.println("Circle: Point is: (" + center.getX() + "," + center.getY()+")");
 	//System.out.println(this.getMessageSource().getMessage("greeting", null, "Default Greeting", null));
-	
+	DrawEvent drawEvent = new DrawEvent(this);
+	drawEvent.publishEvent(drawEvent);
 }
 @PostConstruct
 public void InitializeCircle()
@@ -57,5 +61,10 @@ public void InitializeCircle()
 public void destroyCircle()
 {
 	System.out.println("Destroy of Circle");
+}
+
+public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+	this.publisher = publisher;
+	
 }
 }
---
+++ src/org/korolija/javabrains/MyEventListener.java
@@ -0,0 +1,18 @@
+package org.korolija.javabrains;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyEventListener implements ApplicationListener {
+
+	public void onApplicationEvent(ApplicationEvent event) {
+
+			System.out.println(event.toString());
+		
+	}
+	
+
+}
