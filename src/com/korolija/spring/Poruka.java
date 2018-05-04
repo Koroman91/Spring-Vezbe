aace46af313c20baa98c45f30ec9f12c5a548863
Stefan <korolija91@yahoo.com>
2018-05-04 16:49:18 +0200

    SpringBeanScopeAnnotation

---
+++ src/com/korolija/spring/Poruka.java
@@ -0,0 +1,27 @@
+package com.korolija.spring;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component  // Message message = new Message();
+@Scope("prototype")
+public class Poruka {
+	
+	private int messageId;
+	private String message;
+	
+	public int getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
