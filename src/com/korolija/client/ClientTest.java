9bbdc5607d24671d195a189714e3a00e93521131
Stefan <korolija91@yahoo.com>
2018-05-04 13:21:30 +0200

    ApplicationContext commit

---
+++ src/com/korolija/client/ClientTest.java
@@ -0,0 +1,22 @@
+package com.korolija.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.korolija.context.Employee;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee employee = (Employee) context.getBean("employee", Employee.class);
+		
+		System.out.println(employee.getEmployeeId()+"\t"+employee.getEmployeeName());
+		
+		((AbstractApplicationContext) context).close();
+
+	}
+
+}
