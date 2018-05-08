0b3bd6fa160f37e93af402ce1d76fe60883a968f
Stefan <korolija91@yahoo.com>
2018-05-08 16:40:38 +0200

    SpringBeanLifeCycle commit

---
+++ src/com/korolija/client/ClientTest.java
@@ -0,0 +1,21 @@
+package com.korolija.client;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.korolija.model.Message;
+
+public class ClientTest {
+	
+	public static void main(String [] args)
+	{
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		Message message = ctx.getBean("message", Message.class);
+		
+		System.out.println(message.getMessageId()+"\t"+message.getMessage());
+		
+		ctx.close();
+	}
+
+}
